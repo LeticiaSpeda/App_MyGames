72745100d2c073df43b3716d79bd451a489c287b
Leticia Speda <leticiaspeda@gmail.com>
2023-01-02 10:43:10 -0300

    Feature(NewGameViewController): criado componentes da tela para adicionar informacoes do novo jogo

--- MyGames/MyGames/View/NewGameViewController.swift
+++ MyGames/MyGames/View/NewGameViewController.swift
@@ -9,6 +9,45 @@ import UIKit
 
 final class NewGameViewController: UIViewController {
     
+    private lazy var name: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Digite o nome do jogo"
+        tf.tintColor = .black
+        tf.enableCodeHelper()
+        return tf
+    }()
+    
+    private lazy var platform: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Plataforma"
+        tf.tintColor = .black
+        tf.enableCodeHelper()
+        return tf
+    }()
+    
+    private lazy var dataGame: UILabel = {
+        let label = UILabel()
+        label.text = "Data de Lançamento"
+        label.textColor = .black
+        label.enableCodeHelper()
+        return label
+    }()
+    
+    private lazy var dataCalendar: UIDatePicker = {
+        let calendar = UIDatePicker()
+        calendar.datePickerMode = .date
+        calendar.enableCodeHelper()
+        return calendar
+    }()
+    
+    private lazy var cover: UILabel = {
+        let label = UILabel()
+        label.text = "Capa"
+        label.textColor = .black
+        label.enableCodeHelper()
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         commonInit()
@@ -21,11 +60,22 @@ final class NewGameViewController: UIViewController {
     }
     
     private func configureHierarchy() {
-        
+        view.addSubview(name)
+        view.addSubview(platform)
+        view.addSubview(dataGame)
+        view.addSubview(dataCalendar)
+        view.addSubview(cover)
     }
     
     private func configureConstraints() {
-        
+        NSLayoutConstraint.activate([
+            name.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            platform.topAnchor.constraint(equalTo: name.bottomAnchor, constant: 10),
+            dataGame.topAnchor.constraint(equalTo: platform.bottomAnchor, constant: 10),
+            dataCalendar.topAnchor.constraint(equalTo: dataGame.bottomAnchor, constant: 15),
+            cover.topAnchor.constraint(equalTo: dataCalendar.bottomAnchor, constant: 10)
+            
+        ])
     }
     private func configureStyle() {
         view.backgroundColor = .white
@@ -33,8 +83,8 @@ final class NewGameViewController: UIViewController {
         let appearance = UINavigationBarAppearance()
         appearance.backgroundColor = UIColor(named: "main")
         appearance.titleTextAttributes = [.foregroundColor: UIColor.black]
-        navigationController?.navigationBar.prefersLargeTitles = true
-//        navigationController?.navigationBar.tintColor = .blue
+        navigationController?.navigationBar.prefersLargeTitles = false
+        
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.compactAppearance = appearance
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
@@ -42,4 +92,10 @@ final class NewGameViewController: UIViewController {
     }
     
     
+}
+
+extension UIView {
+    func enableCodeHelper() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
