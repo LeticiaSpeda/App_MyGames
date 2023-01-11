8079976ac20394fc87e075b565d7ab375d6da8b3
Leticia Speda <leticiaspeda@gmail.com>
2023-01-11 20:51:11 -0300

    Feature(UiViewController+Extension): criado um helper com extensao da viewController para usar metodo context

--- MyGames/MyGames/Helpers/UiViewController+Extension.swift
+++ MyGames/MyGames/Helpers/UiViewController+Extension.swift
@@ -5,4 +5,12 @@
 //  Created by Leticia Speda on 06/01/23.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+extension UIViewController {
+    var context: NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }
+}
