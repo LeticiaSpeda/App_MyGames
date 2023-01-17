7c8a715ca318f1a8f6dfadf615e1e5fbdd967a95
Leticia Speda <leticiaspeda@gmail.com>
2023-01-17 15:22:48 -0300

    Feature(ConsolesManager): criado funcao para salvar as informacoes do coreData

--- MyGames/MyGames/View/ConsolesManager.swift
+++ MyGames/MyGames/View/ConsolesManager.swift
@@ -5,4 +5,35 @@
 //  Created by Leticia Speda on 15/01/23.
 //
 
-import Foundation
+import CoreData
+
+final class ConsolesManager {
+    static let shared =  ConsolesManager()
+    var consoles: [Console] = []
+    
+    func loadConsoler(with context: NSManagedObjectContext) {
+        let festRequest: NSFetchRequest<Console> = Console.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        festRequest.sortDescriptors = [sortDescriptor]
+        
+        do {
+          consoles = try context.fetch(festRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func deleteConsole(index: Int, context: NSManagedObjectContext) {
+        let console = consoles[index]
+        context.delete(console)
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private init() {
+        
+    }
+}
