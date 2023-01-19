1dc6f5b33281efb9d111a973cdcc9e5b47aae11b
Leticia Speda <leticiaspeda@gmail.com>
2023-01-19 18:35:36 -0300

    Feature(AddEditViewController+Extension): adicionado extensao para usar o UIImagePickerController e UINavigationController

--- MyGames/MyGames/Helpers/AddEditViewController+Extension.swift
+++ MyGames/MyGames/Helpers/AddEditViewController+Extension.swift
@@ -21,3 +21,13 @@ extension AddEditViewController: UIPickerViewDelegate, UIPickerViewDataSource {
         return console.name
     }
 }
+
+extension AddEditViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        coverImage.image = image
+        imageButton.setTitle(nil, for: .normal)
+        dismiss(animated: true)
+    }
+    
+}
